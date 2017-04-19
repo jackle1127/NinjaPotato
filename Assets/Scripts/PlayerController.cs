63d2d3bcb3d99a9655652b2602f677e85ba53b8c
Madison Hanberry <mhanberry1@gmail.com>
2017-04-19 16:16:56 -0400

    Bleh

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 	public float hp;
 	public int maxInvincibilityFrames;
+	public Slider healthBar;
     private PlayerMovement playerMovement;
 
 	// Use this for initialization
@@ -19,6 +21,7 @@ public class PlayerController : MonoBehaviour {
 	public void TakeDamage(GameObject enemy, float amount, float bounceBackStrength) {
 		if (playerMovement.damageState == 0) {
 			hp -= amount;
+			healthBar.value = hp;
 			Vector3 bounceBack = transform.position - enemy.transform.position;
 			bounceBack = bounceBack.normalized * bounceBackStrength;
 			GetComponent<PlayerMovement> ().TakeDamage (bounceBack);
